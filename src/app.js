f723d4c45cc2842437ab819dd888c7362038883d
hrikbansaha49268 <73658678+hrikbansaha49268@users.noreply.github.com>
2024-04-15 17:28:06 +0530

    Controllers and routers configured

--- src/app.js
+++ src/app.js
@@ -10,4 +10,8 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 
+//Routes import
+import userRoutes from "./routes/user.routes.js";
+app.use("/api/v1/users", userRoutes);
+
 export { app };
---
+++ src/routes/user.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import registerUser from "../controllers/user.controller.js";
+
+const appRouter = Router();
+
+appRouter.route("/register").post(registerUser);
+
+export default appRouter;
