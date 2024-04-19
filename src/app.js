f3cd76bdebb81b6a8d602cd9dfb84fb7a0dac766
hrikbansaha49268 <73658678+hrikbansaha49268@users.noreply.github.com>
2024-04-19 13:56:04 +0530

    All the controllers and routes have been set up.

--- src/app.js
+++ src/app.js
@@ -13,13 +13,55 @@ app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 //Routes import
 
 /* This routes are for Users */
-
 import userRoutes from "./routes/user.routes.js";
 app.use("/api/v1/users", userRoutes);
-
 /* This routes are for Users */
 
+/* This route is for Healthcheck */
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+app.use("/api/v1/healthcheck", healthcheckRouter);
+/* This route is for Healthcheck */
 
+/* This routes are for Tweets */
+import tweetRouter from "./routes/tweet.routes.js"
+app.use("/api/v1/tweets", tweetRouter);
+/* This routes are for Tweets */
+
+
+/* This routes are for Subscribing */
+import subscriptionRouter from "./routes/subscription.routes.js"
+app.use("/api/v1/subscriptions", subscriptionRouter);
+/* This routes are for Subscribing */
+
+
+/* This routes are for Video */
+import videoRouter from "./routes/video.routes.js"
+app.use("/api/v1/videos", videoRouter);
+/* This routes are for Video */
+
+
+/* This routes are for Comments */
+import commentRouter from "./routes/comment.routes.js"
+app.use("/api/v1/comments", commentRouter);
+/* This routes are for Comments */
+
+
+/* This routes are for Likes */
+import likeRouter from "./routes/like.routes.js"
+app.use("/api/v1/likes", likeRouter);
+/* This routes are for Likes */
+
+
+/* This routes are for Playlist */
+import playlistRouter from "./routes/playlist.routes.js"
+app.use("/api/v1/playlist", playlistRouter);
+/* This routes are for Playlist */
+
+
+/* This routes are for dashboard */
+import dashboardRouter from "./routes/dashboard.routes.js"
+app.use("/api/v1/dashboard", dashboardRouter);
+/* This routes are for dashboard */
 
 
 export { app };
