6b9e40f917137a839d81488ce36254498475ec9c
hrikbansaha49268 <73658678+hrikbansaha49268@users.noreply.github.com>
2024-04-19 11:15:21 +0530

    All the models are completed

--- src/app.js
+++ src/app.js
@@ -11,7 +11,15 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 
 //Routes import
+
+/* This routes are for Users */
+
 import userRoutes from "./routes/user.routes.js";
 app.use("/api/v1/users", userRoutes);
+
+/* This routes are for Users */
+
+
+
 
 export { app };
