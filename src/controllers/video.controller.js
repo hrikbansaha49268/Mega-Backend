3ef2554150095bb65827867c5bf6b04c965069a7
hrikbansaha49268 <73658678+hrikbansaha49268@users.noreply.github.com>
2024-04-21 14:20:54 +0530

    Video Controller in progress

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -8,9 +8,23 @@ import { uploadOnCloudinary } from "../utils/fileUpload.js"
 
 
 const getAllVideos = asyncHandler(async (req, res) => {
-    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
     //TODO: get all videos based on query, sort, pagination
-})
+
+    // const allVideos = await VideoSchema.find({});
+    // const videosAggregate = await VideoSchema.aggregate([
+    //     {
+    //         $match: {
+    //             _id: new mongoose.Types.ObjectId(userId)
+    //         }
+    //     }
+    // ]);
+
+    // const videosAggregatePaginated = await VideoSchema.aggregatePaginate(videosAggregate, { page, limit });
+
+    // return res.status(200).json(new ApiResponse(200, videosAggregatePaginated, "Succesfully fetched videos"));
+
+});
 
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description } = req.body;
@@ -23,33 +37,61 @@ const publishAVideo = asyncHandler(async (req, res) => {
         if (!videoFileLocalPath || !thumbnailLocalPath) {
             throw new ApiError(401, "Video File or Thumbnail is missing");
         } else {
-            const videoFileUpload = await uploadOnCloudinary(videoFileLocalPath);
-            const thumbnailUpload = await uploadOnCloudinary(thumbnailLocalPath);
-            const video = await VideoSchema.create({
-                videoFile: videoFileUpload.url,
-                thumbnail: thumbnailUpload.url || "",
-                title: title,
-                description: description,
-                duration: videoFileUpload.duration,
-                owner: req.user?._id
-            });
-            video.save();
-            return res.status(200).json(
-                new ApiResponse(200, video, "Video uploaded successfully")
-            );
+            if (!isValidObjectId(req.user?._id)) {
+                throw new ApiError(401, "Unauthorized request");
+            } else {
+                const videoFileUpload = await uploadOnCloudinary(videoFileLocalPath);
+                const thumbnailUpload = await uploadOnCloudinary(thumbnailLocalPath);
+                const video = await VideoSchema.create({
+                    videoFile: videoFileUpload.url,
+                    thumbnail: thumbnailUpload.url || "",
+                    title: title,
+                    description: description,
+                    duration: videoFileUpload.duration,
+                    owner: req.user?._id
+                });
+                video.save();
+                return res.status(200).json(
+                    new ApiResponse(200, video, "Video uploaded successfully")
+                );
+            }
         };
     };
 });
 
 const getVideoById = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: get video by id
+    const { videoId } = req.params;
+    if (isValidObjectId(videoId)) {
+        const theSpecificVideo = await VideoSchema.findById(videoId);
+        if (!theSpecificVideo) {
+            throw new ApiError(500, "This video does not exists");
+        } else {
+            const aggregatedResponse = await VideoSchema.aggregate([
+                {
+                    $match: {
+                        _id: new mongoose.Types.ObjectId(videoId)
+                    }
+                },
+                {
+                    $lookup: {
+                        from: "users"
+                    }
+                }
+            ]);
+            return res.status(200).json(new ApiResponse
+                (
+                    200,
+                    theSpecificVideo,
+                    "The video is fetched succesfully"
+                ));
+        };
+    };
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
-    //TODO: update video details like title, description, thumbnail
-
+        //TODO: update video details like title, description, thumbnail
+        ``
 })
 
 const deleteVideo = asyncHandler(async (req, res) => {
