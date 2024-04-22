5c51f6e68a4ca31a4c5b9e624b542307b4cad112
Hrikban Saha <hrikbansaha@gmail.com>
2024-04-22 21:26:12 +0530

    VIdeo controller manufactured

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -7,22 +7,94 @@ import asyncHandler from "../utils/asyncHandler.js"
 import { uploadOnCloudinary } from "../utils/fileUpload.js"
 
 const getAllVideos = asyncHandler(async (req, res) => {
-    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
-    //TODO: get all videos based on query, sort, pagination
+    const { page = 1,
+        limit = 10,
+        query = "",
+        sortBy = "createdAt",
+        sortType = 1,
+        userId } = req.query;
 
-    // const allVideos = await VideoSchema.find({});
-    // const videosAggregate = await VideoSchema.aggregate([
-    //     {
-    //         $match: {
-    //             _id: new mongoose.Types.ObjectId(userId)
-    //         }
-    //     }
-    // ]);
+    const matchCondition = {
+        $or: [
+            { title: { $regex: query, $options: "i" } },
+            { description: { $regex: query, $options: "i" } }
+        ]
+    };
+    if (userId) {
+        matchCondition.owner = new mongoose.Types.ObjectId(userId);
+    };
+    var videoAggregate;
+    try {
+        videoAggregate = Video.aggregate([
+            {
+                $match: matchCondition
 
-    // const videosAggregatePaginated = await VideoSchema.aggregatePaginate(videosAggregate, { page, limit });
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "owner",
+                    foreignField: "_id",
+                    as: "owner",
+                    pipeline: [
+                        {
+                            $project: {
+                                _id: 1,
+                                fullName: 1,
+                                avatar: "$avatar.url",
+                                username: 1,
+                            }
+                        },
+                    ]
+                }
+            },
+            {
+                $addFields: {
+                    owner: {
+                        $first: "$owner",
+                    },
+                },
+            },
+            {
+                $sort: {
+                    [sortBy || "createdAt"]: sortType || 1
+                }
+            },
 
-    // return res.status(200).json(new ApiResponse(200, videosAggregatePaginated, "Succesfully fetched videos"));
+        ]);
+    } catch (error) {
+        throw new ApiError(500, error.message || "Internal server error in video aggregation");
+    };
 
+    const options = {
+        page,
+        limit,
+        customLabels: {
+            totalDocs: "totalVideos",
+            docs: "videos",
+
+        },
+        skip: (page - 1) * limit,
+        limit: parseInt(limit),
+    }
+
+    Video.aggregatePaginate(videoAggregate, options)
+        .then(result => {
+            if (result?.videos?.length === 0 && userId) {
+                return res.status(200).json(new ApiResponse(200, [], "No videos found"))
+            }
+
+            return res.status(200)
+                .json(
+                    new ApiResponse(
+                        200,
+                        result,
+                        "video fetched successfully"
+                    )
+                )
+        }).catch(error => {
+            throw new ApiError(500, error?.message || "Internal server error in video aggregate Paginate")
+        });
 });
 
 const publishAVideo = asyncHandler(async (req, res) => {
