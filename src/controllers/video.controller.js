595ed875a098d311a672052a9e1decb3370ae453
Hrikban Saha <hrikbansaha@gmail.com>
2024-04-22 21:14:56 +0530

    Fixed video controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -6,7 +6,6 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import asyncHandler from "../utils/asyncHandler.js"
 import { uploadOnCloudinary } from "../utils/fileUpload.js"
 
-// TODO: This is left
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
     //TODO: get all videos based on query, sort, pagination
